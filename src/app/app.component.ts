2477eafc48cf2476e811813e02c00f3cf00588ec
David Valentino <davidvalen95@gmail.com>
2018-02-26 15:07:37 +0700

    remove test 2 tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,8 +49,8 @@ export class MyApp {
 
 
 
-        this.checkChannel();
-        this.performAutomaticUpdate();
+        // this.checkChannel();
+        // this.performAutomaticUpdate();
 
 
         this.initializeApp();
